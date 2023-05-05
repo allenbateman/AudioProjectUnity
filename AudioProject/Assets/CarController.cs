19e17b9beea94c192611046d6345d13b54300eb1
Allen Bateman <57528826+allenbateman@users.noreply.github.com>
2023-05-05 12:20:40 +0200

    first scene

---
+++ AudioProject/Assets/CarController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Transform tr;
+    public float speed = 20;
+    public float rotationSpeed = 20;
+    public float maxVelocity  = 50;
+    private Quaternion newRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (rb.velocity.magnitude < maxVelocity)
+                rb.velocity += tr.forward * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {    
+
+            if(rb.velocity.magnitude > -maxVelocity )
+                rb.velocity -= tr.forward * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            
+            transform.Rotate(rotationSpeed * tr.up * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(rotationSpeed * -tr.up * Time.deltaTime);
+        }
+    }
+}
