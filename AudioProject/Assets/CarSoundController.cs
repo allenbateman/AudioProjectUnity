282acc4db1ce661918768d47a56fd7c6217fd110
allenbateman <57528826+allenbateman@users.noreply.github.com>
2023-05-10 00:06:24 +0200

    more sounds

--- AudioProject/Assets/CarSoundController.cs
+++ AudioProject/Assets/CarSoundController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CarSoundController : MonoBehaviour
 {
-    AudioSource audioSource;
+    AudioSource engineSound;
     public float minPitch = 0;
     private float pitchFromCar;
     CarController controller;
-
+    public AudioSource driftSound;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.pitch = minPitch;
+        engineSound = GetComponent<AudioSource>();
+        engineSound.pitch = minPitch;
         controller = GetComponent<CarController>();
+        driftSound.volume = 0;
+        driftSound.Play();
     }
 
     // Update is called once per frame
@@ -22,10 +24,41 @@ public class CarSoundController : MonoBehaviour
     {
         pitchFromCar = controller.GetCurrentSpeed() * 0.08f;
         if (pitchFromCar < minPitch)
-            audioSource.pitch = minPitch;
+            engineSound.pitch = minPitch;
         else
-            audioSource.pitch = pitchFromCar;
+            engineSound.pitch = pitchFromCar;
 
- 
+        DriftSound();
+    }
+
+    void DriftSound()
+    {
+        if (controller.GetCurrentSpeed() < 35)
+        {
+            driftSound.volume = 0;
+            return;
+        }
+
+        float dot = Vector3.Dot(controller.transform.forward.normalized, controller.GetComponent<Rigidbody>().velocity.normalized);
+
+        if (dot > 1)
+            dot = 1;
+
+        driftSound.volume = (1 - dot) * 1.6f;
+
+
+        Debug.Log(1 - dot);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "wall")
+        {
+            if(controller.speed > 35)
+            {
+                //crash
+            }
+            //crash
+        }
     }
 }
